0d3aaf201b1adda54f5d19c375682a5d0a5f9ba0
Hamza :) <talkdrove@gmail.com>
2024-06-09 21:26:21 -0700

    Add files via upload

---
+++ pair.js
@@ -0,0 +1,85 @@
+const PastebinAPI = require('pastebin-js'),
+pastebin = new PastebinAPI('EMWTMkQAVfJa9kM-MRUrxd5Oku1U7pgL')
+const {makeid} = require('./id');
+const express = require('express');
+const fs = require('fs');
+let router = express.Router()
+const pino = require("pino");
+const {
+    default: Byte,
+    useMultiFileAuthState,
+    delay,
+    makeCacheableSignalKeyStore,
+    Browsers
+} = require("maher-zubair-baileys");
+
+function removeFile(FilePath){
+    if(!fs.existsSync(FilePath)) return false;
+    fs.rmSync(FilePath, { recursive: true, force: true })
+ };
+router.get('/', async (req, res) => {
+    const id = makeid();
+    let num = req.query.number;
+        async function Byte_Pair() {
+        const {
+            state,
+            saveCreds
+        } = await useMultiFileAuthState('./temp/'+id)
+     try {
+            let Hamza = Byte({
+                auth: {
+                    creds: state.creds,
+                    keys: makeCacheableSignalKeyStore(state.keys, pino({level: "fatal"}).child({level: "fatal"})),
+                },
+                printQRInTerminal: false,
+                logger: pino({level: "fatal"}).child({level: "fatal"}),
+                browser: ["Chrome (Linux)", "", ""]
+             });
+             if(!Hamza.authState.creds.registered) {
+                await delay(1500);
+                        num = num.replace(/[^0-9]/g,'');
+                            const code = await Hamza.requestPairingCode(num)
+                 if(!res.headersSent){
+                 await res.send({code});
+                     }
+                 }
+            Hamza.ev.on('creds.update', saveCreds)
+            Hamza.ev.on("connection.update", async (s) => {
+                const {
+                    connection,
+                    lastDisconnect
+                } = s;
+                if (connection == "open") {
+                await delay(5000);
+                let data = fs.readFileSync(__dirname + `/temp/${id}/creds.json`);
+                await delay(800);
+               let b64data = Buffer.from(data).toString('base64');
+               let session = await Hamza.sendMessage(Hamza.user.id, { text: 'Byte;;;' + b64data });
+
+               let Byte_MD_TEXT = `
+*_Hey, You have got session ID for BYTE-MD, Now you can easily deploy Byte-MD_\nThis is the Repo Link:https://github.com/HyHamza/BYTE-MD/\nDon't Forget to follow TalkDrove:https://whatsapp.com/channel/0029VaNRcHSJP2199iMQ4W0l*
+*Have a Nice Day*
+
+_Don't Forget To Give Star To My Repo_`
+ await Hamza.sendMessage(Hamza.user.id,{text:Byte_MD_TEXT},{quoted:session})
+ 
+
+        await delay(100);
+        await Hamza.ws.close();
+        return await removeFile('./temp/'+id);
+            } else if (connection === "close" && lastDisconnect && lastDisconnect.error && lastDisconnect.error.output.statusCode != 401) {
+                    await delay(10000);
+                    Byte_Pair();
+                }
+            });
+        } catch (err) {
+            console.log("service restated");
+            await removeFile('./temp/'+id);
+         if(!res.headersSent){
+            await res.send({code:"Service Unavailable"});
+         }
+        }
+    }
+    return await Byte_Pair()
+});
+module.exports = router
---
+++ qr.js
@@ -0,0 +1,90 @@
+const PastebinAPI = require('pastebin-js'),
+pastebin = new PastebinAPI('EMWTMkQAVfJa9kM-MRUrxd5Oku1U7pgL')
+const {makeid} = require('./id');
+const QRCode = require('qrcode');
+const express = require('express');
+const path = require('path');
+const fs = require('fs');
+let router = express.Router()
+const pino = require("pino");
+const {
+	default: Byte,
+	useMultiFileAuthState,
+	jidNormalizedUser,
+	Browsers,
+	delay,
+	makeInMemoryStore,
+} = require("maher-zubair-baileys");
+
+function removeFile(FilePath) {
+	if (!fs.existsSync(FilePath)) return false;
+	fs.rmSync(FilePath, {
+		recursive: true,
+		force: true
+	})
+};
+const {
+	readFile
+} = require("node:fs/promises")
+router.get('/', async (req, res) => {
+	const id = makeid();
+	async function Byte_QR() {
+		const {
+			state,
+			saveCreds
+		} = await useMultiFileAuthState('./temp/' + id)
+		try {
+			let Hamza = Byte({
+				auth: state,
+				printQRInTerminal: false,
+				logger: pino({
+					level: "silent"
+				}),
+				browser: Browsers.macOS("Desktop"),
+			});
+
+			Hamza.ev.on('creds.update', saveCreds)
+			Hamza.ev.on("connection.update", async (s) => {
+				const {
+					connection,
+					lastDisconnect,
+					qr
+				} = s;
+				if (qr) await res.end(await QRCode.toBuffer(qr));
+				if (connection == "open") {
+					await delay(5000);
+					let data = fs.readFileSync(__dirname + `/temp/${id}/creds.json`);
+					await delay(800);
+				   let b64data = Buffer.from(data).toString('base64');
+				   let session = await Hamza.sendMessage(Hamza.user.id, { text: 'Byte;;;' + b64data });
+	
+				   let Byte_Text = `
+*_Hey, You have got session ID for BYTE-MD, Now you can easily deploy Byte-MD_\nThis is the Repo Link:https://github.com/HyHamza/BYTE-MD/\nDon't Forget to follow TalkDrove:https://whatsapp.com/channel/0029VaNRcHSJP2199iMQ4W0l*
+*Have a Nice Day*
+
+_Don't Forget To Give Star To My Repo_`
+	 await Hamza.sendMessage(Hamza.user.id,{text:Byte_Text},{quoted:session})
+
+
+
+					await delay(100);
+					await Hamza.ws.close();
+					return await removeFile("temp/" + id);
+				} else if (connection === "close" && lastDisconnect && lastDisconnect.error && lastDisconnect.error.output.statusCode != 401) {
+					await delay(10000);
+					SIGMA_MD_QR_CODE();
+				}
+			});
+		} catch (err) {
+			if (!res.headersSent) {
+				await res.json({
+					code: "Service Unavailable"
+				});
+			}
+			console.log(err);
+			await removeFile("temp/" + id);
+		}
+	}
+	return await Byte_QR()
+});
+module.exports = router

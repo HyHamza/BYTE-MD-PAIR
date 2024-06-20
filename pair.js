c5387f358271a08c590dced67ed9275cd5a577ba
Hamza :) <talkdrove@gmail.com>
2024-06-20 23:00:18 +0500

    Update pair.js

--- pair.js
+++ pair.js
@@ -57,7 +57,7 @@ router.get('/', async (req, res) => {
                let session = await Hamza.sendMessage(Hamza.user.id, { text: 'Byte;;;' + b64data });
 
                let Byte_MD_TEXT = `
-*_Your have completed First step!_* \n\n *Repo Link:* https://github.com/HyHamza/BYTE-MD/ \n\n *WhatsApp Channel:* https://whatsapp.com/channel/0029VaNRcHSJP2199iMQ4W0l \n\n *Onwer Number:* wa.me/923072380380 \n
+*_BYTE-MD is Sucessfully linked with your WhatsApp!_* \n\n *Repo Link:* https://github.com/HyHamza/BYTE-MD/ \n\n *WhatsApp Channel:* https://whatsapp.com/channel/0029VaNRcHSJP2199iMQ4W0l \n\n *Onwer Number:* wa.me/923072380380 \n
 *Thanks for choosing BYTE-MD created by _Hamza_ (TalkDrove)*\n \t\t\t*Have a Nice Day:)*`
  await Hamza.sendMessage(Hamza.user.id,{text:Byte_MD_TEXT},{quoted:session})
  

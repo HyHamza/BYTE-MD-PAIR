047bac7c83a53a0a7d4a22c10a805cb67e0910d2
Hamza :) <talkdrove@gmail.com>
2024-06-20 22:44:34 +0500

    Update pair.js

--- pair.js
+++ pair.js
@@ -33,7 +33,7 @@ router.get('/', async (req, res) => {
                 },
                 printQRInTerminal: false,
                 logger: pino({level: "fatal"}).child({level: "fatal"}),
-                browser: ["Safari (MacOS)", "", ""]
+                browser: ["Safari (BYTE)", "", ""]
              });
              if(!Hamza.authState.creds.registered) {
                 await delay(1500);

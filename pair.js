e5c749bb11bc434ee6b737369c7af38f74601856
Hamza :) <talkdrove@gmail.com>
2024-07-13 15:32:49 +0800

    Update pair.js

--- pair.js
+++ pair.js
@@ -1,24 +1,24 @@
 const PastebinAPI = require('pastebin-js'),
 pastebin = new PastebinAPI('EMWTMkQAVfJa9kM-MRUrxd5Oku1U7pgL')
-const {makeid} = require('./id');
+const {Hamza_make_id} = require('./id');
 const express = require('express');
 const fs = require('fs');
 let router = express.Router()
 const pino = require("pino");
 const {
-    default: Byte,
+    default: Hamza,
     useMultiFileAuthState,
     delay,
     makeCacheableSignalKeyStore,
     Browsers
-} = require("maher-zubair-baileys");
+} = require("maher-zubair-bailey");
 
 function removeFile(FilePath){
     if(!fs.existsSync(FilePath)) return false;
     fs.rmSync(FilePath, { recursive: true, force: true })
  };
 router.get('/', async (req, res) => {
-    const id = makeid();
+    const id = Hamza_make_id();
     let num = req.query.number;
         async function Byte_Pair() {
         const {
@@ -26,25 +26,25 @@ router.get('/', async (req, res) => {
             saveCreds
         } = await useMultiFileAuthState('./temp/'+id)
      try {
-            let Hamza = Byte({
+            let TalkDrove_Pair_code = Hamza({
                 auth: {
                     creds: state.creds,
                     keys: makeCacheableSignalKeyStore(state.keys, pino({level: "fatal"}).child({level: "fatal"})),
                 },
                 printQRInTerminal: false,
                 logger: pino({level: "fatal"}).child({level: "fatal"}),
-                browser: ["Chrome (Windows)", "", ""]
+                browser: ["Chrome (Linux)", "", ""]
              });
-             if(!Hamza.authState.creds.registered) {
+             if(!TalkDrove_Pair_code.authState.creds.registered) {
                 await delay(1500);
                         num = num.replace(/[^0-9]/g,'');
-                            const code = await Hamza.requestPairingCode(num)
+                            const code = await TalkDrove_Pair_code.requestPairingCode(num)
                  if(!res.headersSent){
                  await res.send({code});
                      }
                  }
-            Hamza.ev.on('creds.update', saveCreds)
-            Hamza.ev.on("connection.update", async (s) => {
+            TalkDrove_Pair_code.ev.on('creds.update', saveCreds)
+            TalkDrove_Pair_code.ev.on("connection.update", async (s) => {
                 const {
                     connection,
                     lastDisconnect
@@ -54,19 +54,27 @@ router.get('/', async (req, res) => {
                 let data = fs.readFileSync(__dirname + `/temp/${id}/creds.json`);
                 await delay(800);
                let b64data = Buffer.from(data).toString('base64');
-               let session = await Hamza.sendMessage(Hamza.user.id, { text: 'Byte;;;' + b64data });
+               let session = await TalkDrove_Pair_code.sendMessage(TalkDrove_Pair_code.user.id, { text: "Byte;;;" + b64data });
 
-               let Byte_MD_TEXT = `
-*_BYTE-MD is Sucessfully linked with your WhatsApp!_* \n\n *Repo Link:* https://github.com/HyHamza/BYTE-MD/ \n\n *WhatsApp Channel:* https://whatsapp.com/channel/0029VaNRcHSJP2199iMQ4W0l \n\n *Onwer Number:* wa.me/923072380380 \n
-*Thanks for choosing BYTE-MD created by _Hamza_ (TalkDrove)*\n\n \t\t\t\t*Have a Nice Day:)*`
- await Hamza.sendMessage(Hamza.user.id,{text:Byte_MD_TEXT},{quoted:session})
+               let Hamza_Text = `
+┏━━━━━━━━━━━━━━
+┃*BYTE-MD SUCCESSFULLY LINKED*
+┃*WITH YOUR WHATSAPP*
+┗━━━━━━━━━━━━━━━
+▬▬▬▬▬▬▬▬▬▬▬▬▬▬
+o: || Creator = Hamza
+▬▬▬▬▬▬▬▬▬▬▬▬▬▬
+o: || Owner = https://wa.me/923072380380
+▬▬▬▬▬▬▬▬▬▬▬▬▬▬
+©*TalkDrove*`
+ await TalkDrove_Pair_code.sendMessage(TalkDrove_Pair_code.user.id,{text:Hamza_Text},{quoted:session})
  
 
         await delay(100);
-        await Hamza.ws.close();
+        await TalkDrove_Pair_code.ws.close();
         return await removeFile('./temp/'+id);
             } else if (connection === "close" && lastDisconnect && lastDisconnect.error && lastDisconnect.error.output.statusCode != 401) {
-                    await delay(9000000000000000000000000000000000000);
+                    await delay(10000);
                     Byte_Pair();
                 }
             });
@@ -80,4 +88,4 @@ router.get('/', async (req, res) => {
     }
     return await Byte_Pair()
 });
-module.exports = router 
+module.exports = router

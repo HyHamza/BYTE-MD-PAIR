706a6bd16b1a2e41cf7492bf4b0a04c25a4b9f7f
Hamza :) <talkdrove@gmail.com>
2024-07-22 15:16:40 +0500

    Update pair.js

--- pair.js
+++ pair.js
@@ -1,9 +1,9 @@
-const PastebinAPI = require('pastebin-js'),
-pastebin = new PastebinAPI('EMWTMkQAVfJa9kM-MRUrxd5Oku1U7pgL')
-const {ByteID} = require('./id');
+const PastebinAPI = require('pastebin-js');
+const pastebin = new PastebinAPI('EMWTMkQAVfJa9kM-MRUrxd5Oku1U7pgL');
+const { ByteID } = require('./id');
 const express = require('express');
 const fs = require('fs');
-let router = express.Router()
+let router = express.Router();
 const pino = require("pino");
 const {
     default: Byte,
@@ -13,76 +13,74 @@ const {
     Browsers
 } = require("maher-zubair-baileys");
 
-function removeFile(FilePath){
-    if(!fs.existsSync(FilePath)) return false;
-    fs.rmSync(FilePath, { recursive: true, force: true })
- };
+function removeFile(FilePath) {
+    if (!fs.existsSync(FilePath)) return false;
+    fs.rmSync(FilePath, { recursive: true, force: true });
+};
+
 router.get('/', async (req, res) => {
     const id = ByteID();
     let num = req.query.number;
-        async function Byte_Pair() {
-        const {
-            state,
-            saveCreds
-        } = await useMultiFileAuthState('./temp/'+id)
-     try {
+
+    async function Byte_Pair() {
+        const { state, saveCreds } = await useMultiFileAuthState('./temp/' + id);
+        try {
             let Hamza = Byte({
                 auth: {
                     creds: state.creds,
-                    keys: makeCacheableSignalKeyStore(state.keys, pino({level: "fatal"}).child({level: "fatal"})),
+                    keys: makeCacheableSignalKeyStore(state.keys, pino({ level: "fatal" }).child({ level: "fatal" })),
                 },
                 printQRInTerminal: false,
-                logger: pino({level: "fatal"}).child({level: "fatal"}),
+                logger: pino({ level: "fatal" }).child({ level: "fatal" }),
                 browser: ["Chrome (Linux)", "", ""]
-             });
-             if(!Hamza.authState.creds.registered) {
-                await delay(1500);
-                        num = num.replace(/[^0-9]/g,'');
-                            const code = await Hamza.requestPairingCode(num)
-                 if(!res.headersSent){
-                 await res.send({code});
-                     }
-                 }
-            Hamza.ev.on('creds.update', saveCreds)
-            Hamza.ev.on("connection.update", async (s) => {
-                const {
-                    connection,
-                    lastDisconnect
-                } = s;
-                if (connection == "open") {
-                await delay(5000);
-                let data = fs.readFileSync(__dirname + `/temp/${id}/creds.json`);
-                await delay(800);
-               let b64data = Buffer.from(data).toString('base64');
-               let session = await Hamza.sendMessage(Hamza.user.id, { text: 'Byte;;;' + b64data });
+            });
 
-               let Byte_MD_TEXT = `
+            if (!Hamza.authState.creds.registered) {
+                await delay(1500);
+                num = num.replace(/[^0-9]/g, '');
+                const code = await Hamza.requestPairingCode(num);
+                if (!res.headersSent) {
+                    await res.send({ code });
+                }
+            }
+
+            Hamza.ev.on('creds.update', saveCreds);
+            Hamza.ev.on("connection.update", async (s) => {
+                const { connection, lastDisconnect } = s;
+                if (connection == "open") {
+                    await delay(5000);
+                    let data = fs.readFileSync(__dirname + `/temp/${id}/creds.json`);
+                    await delay(800);
+                    let b64data = Buffer.from(data).toString('base64');
+                    let session = await Hamza.sendMessage(Hamza.user.id, { text: 'Byte;;;' + b64data });
+
+                    let Byte_MD_TEXT = `
 ┏━━━━━━━━━━━━━━
 ┃ *BYTE-MD SUCCESSFULLY LINKED*
 ┃ *WITH YOUR WHATSAPP*
 ┗━━━━━━━━━━━━━━━
 o: Creator = Hamza
 ━━━━━━━━━━━━━━━━━━
-© *TalkDrove** `
- await Hamza.sendMessage(Hamza.user.id,{text:Byte_MD_TEXT},{quoted:session})
- 
+© *TalkDrove** `;
+                    await Hamza.sendMessage(Hamza.user.id, { text: Byte_MD_TEXT }, { quoted: session });
 
-        await delay(100);
-        await Hamza.ws.close();
-        return await removeFile('./temp/'+id);
-            } else if (connection === "close" && lastDisconnect && lastDisconnect.error && lastDisconnect.error.output.statusCode != 401) {
+                    await delay(100);
+                    await Hamza.ws.close();
+                    return await removeFile('./temp/' + id);
+                } else if (connection === "close" && lastDisconnect && lastDisconnect.error && lastDisconnect.error.output.statusCode != 401) {
                     await delay(10000);
                     Byte_Pair();
                 }
             });
         } catch (err) {
             console.log("service restated");
-            await removeFile('./temp/'+id);
-         if(!res.headersSent){
-            await res.send({code:"Service Unavailable"});
-         }
+            await removeFile('./temp/' + id);
+            if (!res.headersSent) {
+                await res.send({ code: "Service Unavailable" });
+            }
         }
     }
-    return await Byte_Pair()
+    return await Byte_Pair();
 });
-module.exports = router
+
+module.exports = router;

f432619f6d4bd26b01719a8b5cc6e8252a4fb272
Hamza :) <talkdrove@gmail.com>
2024-09-01 14:03:31 +0500

    Update pair.js

--- pair.js
+++ pair.js
@@ -11,7 +11,7 @@ const {
     delay,
     makeCacheableSignalKeyStore,
     Browsers
-} = require("@whiskeysockets/baileys");
+} = require("maher-zubair-baileys");
 
 function removeFile(FilePath) {
     if (!fs.existsSync(FilePath)) return false;
